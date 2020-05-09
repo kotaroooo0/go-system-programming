b67b7f59a5a532016702351515440da9dda106b6
kotaroooo0 <k33asby@gmail.com>
2020-05-10 01:26:25 +0900

    TCPソケットを使ったサーバーとクライアント

---
+++ chapter6/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.NewRequest("GET", "http://localhost:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Write(conn)
+	res, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpResponse(res, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+}
